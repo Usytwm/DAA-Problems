792a73aced938227106b3d9eb25bcd382a830219
brian <brian@jobs2live.com>
2024-09-21 22:42:57 -0400

    refactorizando el codigo

--- Problem 3/code/approximate_solution.py
+++ Problem 3/code/approximate_solution.py
@@ -2,26 +2,6 @@ import heapq
 import random
 
 
-def generate_random_graph(num_nodos, probabilidad_conexion):
-    """
-    Genera un grafo aleatorio utilizando un número de nodos y una probabilidad de conexión.
-    ---
-    :param num_nodos: Número de nodos en el grafo.
-    :param probabilidad_conexion: Probabilidad de que dos nodos estén conectados.
-    :return: Un diccionario que representa el grafo.
-    """
-    grafo = {i: [] for i in range(num_nodos)}
-
-    for i in range(num_nodos):
-        for j in range(i + 1, num_nodos):
-            if random.random() < probabilidad_conexion:
-                # (grafo no dirigido)
-                grafo[i].append(j)
-                grafo[j].append(i)
-
-    return grafo
-
-
 def greedy_dominating_set(graph):
     """
     Implementa el algoritmo greedy para encontrar un conjunto dominante aproximado en un grafo.
@@ -48,6 +28,26 @@ def greedy_dominating_set(graph):
                 dominated.add(neighbor)
 
     return dominating_set
+
+
+def generate_random_graph(num_nodos, probabilidad_conexion):
+    """
+    Genera un grafo aleatorio utilizando un número de nodos y una probabilidad de conexión.
+    ---
+    :param num_nodos: Número de nodos en el grafo.
+    :param probabilidad_conexion: Probabilidad de que dos nodos estén conectados.
+    :return: Un diccionario que representa el grafo.
+    """
+    grafo = {i: [] for i in range(num_nodos)}
+
+    for i in range(num_nodos):
+        for j in range(i + 1, num_nodos):
+            if random.random() < probabilidad_conexion:
+                # (grafo no dirigido)
+                grafo[i].append(j)
+                grafo[j].append(i)
+
+    return grafo
 
 
 def test_greedy_dominating_set(num_nodos, probabilidad_conexion):
