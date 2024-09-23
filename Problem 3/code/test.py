2722f267bdb6e5c782abb38a634c27b6da824b15
brian <brian@jobs2live.com>
2024-09-23 00:40:36 -0400

    refactorizando test

--- Problem 3/code/test.py
+++ Problem 3/code/test.py
@@ -72,58 +72,7 @@ def compare_solutions(num_grafos, probabilidad_conexion=0.5):
     )
 
 
-# # Función para graficar los resultados
-# def plot_results(
-#     num_grafos, exact_solution_sizes, approximate_solution_sizes, max_degrees
-# ):
-#     x = list(range(1, num_grafos + 1))  # Eje X: número de grafo
-
-#     # Primer gráfico: Comparación de las soluciones exactas y aproximadas
-#     plt.figure(figsize=(10, 6))
-#     plt.plot(x, exact_solution_sizes, label="Solución Exacta", color="blue")
-#     plt.plot(
-#         x,
-#         approximate_solution_sizes,
-#         label="Solución Aproximada",
-#         color="green",
-#     )
-
-#     plt.title("Comparación de soluciones exactas y aproximadas")
-#     plt.xlabel("Número de grafo")
-#     plt.ylabel("Tamaño del conjunto dominante")
-#     plt.legend()
-#     plt.grid(True)
-#     plt.show()
-
-#     # Segundo gráfico: Comparación del logaritmo del grado máximo y la diferencia entre aproximada y exacta
-#     aprox = [approx for approx in approximate_solution_sizes]
-
-#     # Calcular el logaritmo de los grados máximos
-#     log_max_degrees = [
-#         math.ceil(math.log(degree + 2)) * exact_solution_sizes[index]
-#         for index, degree in enumerate(max_degrees)
-#     ]
-
-#     # Gráfico de logaritmo del grado máximo y diferencias entre aproximada y exacta
-#     plt.figure(figsize=(10, 6))
-#     plt.plot(
-#         x,
-#         log_max_degrees,
-#         label="Log(Grado Máximo) * Solucion Exacta",
-#         color="orange",
-#     )
-#     plt.plot(x, aprox, label=" Solucion Aproximada", color="red")
-
-
-#     plt.title(
-#         "Logaritmo del Grado Máximo y diferencia entre soluciones aproximadas y exactas"
-#     )
-#     plt.xlabel("Número de grafo")
-#     plt.ylabel("Valor")
-#     plt.legend()
-#     plt.grid(True)
-#     plt.show()
-# Función para graficar los resultados
+# graficar los resultados
 def plot_results(
     num_grafos, exact_solution_sizes, approximate_solution_sizes, max_degrees
 ):
@@ -184,24 +133,3 @@ def plot_results(
 
 
 compare_solutions(150, 0.4)
-
-# # graph = {
-# #     0: [1, 7],  # A -> B, H
-# #     1: [0, 2, 7],  # B -> A, C, H
-# #     2: [1, 3, 4, 5, 7],  # C -> B, D, E, F, H
-# #     3: [2, 5, 6],  # D -> C, F, G
-# #     4: [2, 5],  # E -> C, F
-# #     5: [2, 3, 4, 6],  # F -> C, D, E, G
-# #     6: [3, 5, 7],  # G -> D, F, H
-# #     7: [0, 1, 2, 6],  # H -> A, B, C, G
-# # }
-# graph = {0: [2, 4], 1: [4, 5], 2: [0, 5], 3: [4, 5], 4: [0, 1, 3], 5: [1, 2, 3]}
-# # # graph = {
-# # #     0: [1],
-# # #     1: [0],
-# # # }
-# graph = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
-
-
-# greedy = greedy_dominating_set(graph)
-# print(greedy)
